465958268ad70e8c9e8007c74f5383319e2120ed
HexMakina <20891495+HexMakina@users.noreply.github.com>
2026-04-01 18:15:06 +0200

    cleanup and reset for evening class

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 
-<?php require 'database.php'?>
+<?php require 'database.php'; ?>
 <?php include 'top.html';?>
 
 <main class="container">
 <?php 
-if(empty($_GET)){
+if(empty($_GET["category"])){
     $page = 'home';
 }
-elseif(isset($_GET["category"])){
+else{
     $page = $_GET["category"];
-}
-elseif(isset($_GET['item'])){
-    $page = 'item';
 }
 
 include $page . '.php';
